c75f2cd98bc91721543766bac937f2c01c059ba3
ybbarng <ybbarng@gmail.com>
2015-10-12 00:19:09 +0900

    permutation 구현

---
+++ cube.py
@@ -0,0 +1,47 @@
+from collections import defaultdict
+
+
+class Block:
+    def __init__(self, color_orders, *colors):
+        self.index = sum(color_orders[color] for color in colors)
+
+    def __repr__(self):
+        return str(self.index)
+
+
+class Cube:
+    blocks = []
+    solved = []
+
+    def __init__(self, i):
+        color_orders = defaultdict(int)
+        up = [i[0][0], i[0][1], i[1][0], i[1][1]]
+        front = [i[2][0], i[2][1], i[3][0], i[3][1]]
+        right = [i[2][3], i[2][4], i[3][3], i[3][4]]
+        back = [i[2][6], i[2][7], i[3][6], i[3][7]]
+        left = [i[2][9], i[2][10], i[3][9], i[3][10]]
+        down = [i[4][0], i[4][1], i[5][0], i[5][1]]
+        color_orders[up[3]] = 4
+        color_orders[front[1]] = 2
+        color_orders[right[0]] = 1
+
+        self.blocks.append(Block(color_orders, down[2], left[2], back[3]))
+        self.blocks.append(Block(color_orders, down[3], back[2], right[3]))
+        self.blocks.append(Block(color_orders, down[0], front[2], left[3]))
+        self.blocks.append(Block(color_orders, down[1], right[2], front[3]))
+        self.blocks.append(Block(color_orders, up[0], back[1], left[0]))
+        self.blocks.append(Block(color_orders, up[1], right[1], back[0]))
+        self.blocks.append(Block(color_orders, up[2], left[1], front[0]))
+        self.blocks.append(Block(color_orders, up[3], front[1], right[0]))
+        self.solved = sorted(self.blocks, key=lambda x: x.index)
+        print(self.blocks)
+
+
+if __name__ == '__main__':
+    Cube('''OO
+OO
+RR GG BB WW
+RR GG BB WW
+YY
+YY
+'''.split('\n'))
