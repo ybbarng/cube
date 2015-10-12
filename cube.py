7710fed16b82d8b5beb02e387bf0f30d85c0e744
ybbarng <ybbarng@gmail.com>
2015-10-12 21:08:28 +0900

    Cube class 제거

--- cube.py
+++ cube.py
@@ -24,96 +24,83 @@ class Block:
                 break
 
 
-class Cube:
+def get_cube(i):
+    blocks = []
+    solved = []
+    color_orders = defaultdict(int)
+    up = [i[0][0], i[0][1], i[1][0], i[1][1]]
+    front = [i[2][0], i[2][1], i[3][0], i[3][1]]
+    right = [i[2][3], i[2][4], i[3][3], i[3][4]]
+    back = [i[2][6], i[2][7], i[3][6], i[3][7]]
+    left = [i[2][9], i[2][10], i[3][9], i[3][10]]
+    down = [i[4][0], i[4][1], i[5][0], i[5][1]]
+    color_orders[up[3]] = 4
+    color_orders[front[1]] = 2
+    color_orders[right[0]] = 1
 
-    def __init__(self, blocks=None, i=None):
-        if blocks is not None:
-            self.blocks = blocks
-            return
+    blocks.append(Block(color_orders, colors=(down[2], left[2], back[3])))
+    blocks.append(Block(color_orders, colors=(down[3], back[2], right[3])))
+    blocks.append(Block(color_orders, colors=(down[0], front[2], left[3])))
+    blocks.append(Block(color_orders, colors=(down[1], right[2], front[3])))
+    blocks.append(Block(color_orders, colors=(up[0], back[1], left[0])))
+    blocks.append(Block(color_orders, colors=(up[1], right[1], back[0])))
+    blocks.append(Block(color_orders, colors=(up[2], left[1], front[0])))
+    blocks.append(Block(color_orders, colors=(up[3], front[1], right[0])))
+    solved = sorted(blocks, key=lambda x: x.index)
+    bottom_colors = Counter()
+    for i in range(0, 4):
+        bottom_colors.update(solved[i].colors)
+    bottom_color = bottom_colors.most_common(1)[0][0]
+    top_color = up[3]
+    ret = []
+    for block in blocks:
+        block.set_orientation(top_color, bottom_color)
+        ret.append(block.index)
+        ret.append(block.orientation)
+    return tuple(ret)
 
-        blocks = []
-        solved = []
-        color_orders = defaultdict(int)
-        up = [i[0][0], i[0][1], i[1][0], i[1][1]]
-        front = [i[2][0], i[2][1], i[3][0], i[3][1]]
-        right = [i[2][3], i[2][4], i[3][3], i[3][4]]
-        back = [i[2][6], i[2][7], i[3][6], i[3][7]]
-        left = [i[2][9], i[2][10], i[3][9], i[3][10]]
-        down = [i[4][0], i[4][1], i[5][0], i[5][1]]
-        color_orders[up[3]] = 4
-        color_orders[front[1]] = 2
-        color_orders[right[0]] = 1
 
-        blocks.append(Block(color_orders, colors=(down[2], left[2], back[3])))
-        blocks.append(Block(color_orders, colors=(down[3], back[2], right[3])))
-        blocks.append(Block(color_orders, colors=(down[0], front[2], left[3])))
-        blocks.append(Block(color_orders, colors=(down[1], right[2], front[3])))
-        blocks.append(Block(color_orders, colors=(up[0], back[1], left[0])))
-        blocks.append(Block(color_orders, colors=(up[1], right[1], back[0])))
-        blocks.append(Block(color_orders, colors=(up[2], left[1], front[0])))
-        blocks.append(Block(color_orders, colors=(up[3], front[1], right[0])))
-        solved = sorted(blocks, key=lambda x: x.index)
-        bottom_colors = Counter()
-        for i in range(0, 4):
-            bottom_colors.update(solved[i].colors)
-        bottom_color = bottom_colors.most_common(1)[0][0]
-        top_color = up[3]
-        self.blocks = []
-        for block in blocks:
-            block.set_orientation(top_color, bottom_color)
-            self.blocks.append([block.index, block.orientation])
+def turn_base(cube_tuple, moves, rotations):
+    new_state = []
+    for move in moves:
+        new_state.append(cube_tuple[move * 2])
+        new_state.append(cube_tuple[move * 2 + 1])
 
-    @staticmethod
-    def turn_base(cube_tuple, moves, rotations):
-        new_state = []
-        for move in moves:
-            new_state.append(cube_tuple[move * 2])
-            new_state.append(cube_tuple[move * 2 + 1])
+    for i, rotation in enumerate(rotations):
+        rotate(new_state, i * 2 + 1, rotation)
+    return tuple(new_state)
 
-        for i, rotation in enumerate(rotations):
-            Cube.rotate(new_state, i * 2 + 1, rotation)
-        return tuple(new_state)
 
-    @staticmethod
-    def d(cube_tuple):
-        return Cube.turn_base(cube_tuple, [1, 3, 0, 2, 4, 5, 6, 7], [0, 0, 0, 0, 0, 0, 0, 0])
+def d(cube_tuple):
+    return turn_base(cube_tuple, [1, 3, 0, 2, 4, 5, 6, 7], [0, 0, 0, 0, 0, 0, 0, 0])
 
-    @staticmethod
-    def dr(cube_tuple):
-        return Cube.turn_base(cube_tuple, [2, 0, 3, 1, 4, 5, 6, 7], [0, 0, 0, 0, 0, 0, 0, 0])
 
-    @staticmethod
-    def l(cube_tuple):
-        return Cube.turn_base(cube_tuple, [2, 1, 6, 3, 0, 5, 4, 7], [2, 0, 1, 0, 1, 0, 2, 0])
+def dr(cube_tuple):
+    return turn_base(cube_tuple, [2, 0, 3, 1, 4, 5, 6, 7], [0, 0, 0, 0, 0, 0, 0, 0])
 
-    @staticmethod
-    def lr(cube_tuple):
-        return Cube.turn_base(cube_tuple, [4, 1, 0, 3, 6, 5, 2, 7], [2, 0, 1, 0, 1, 0, 2, 0])
 
-    @staticmethod
-    def b(cube_tuple):
-        return Cube.turn_base(cube_tuple, [4, 0, 2, 3, 5, 1, 6, 7], [1, 2, 0, 0, 2, 1, 0, 0])
+def l(cube_tuple):
+    return turn_base(cube_tuple, [2, 1, 6, 3, 0, 5, 4, 7], [2, 0, 1, 0, 1, 0, 2, 0])
 
-    @staticmethod
-    def br(cube_tuple):
-        return Cube.turn_base(cube_tuple, [1, 5, 2, 3, 0, 4, 6, 7], [1, 2, 0, 0, 2, 1, 0, 0])
 
-    @staticmethod
-    def rotate(state, i, rotation):
-        state[i] = (state[i] + rotation) % 3
+def lr(cube_tuple):
+    return turn_base(cube_tuple, [4, 1, 0, 3, 6, 5, 2, 7], [2, 0, 1, 0, 1, 0, 2, 0])
 
-    @classmethod
-    def from_tuple(cls, cube_list):
-        blocks = [[cube_list[i * 2], cube_list[i * 2 + 1]] for i in range(0, 8)]
-        cube = cls(blocks=blocks)
-        return cube
 
-    def to_tuple(self):
-        return tuple(_ for block in self.blocks for _ in block)
+def b(cube_tuple):
+    return turn_base(cube_tuple, [4, 0, 2, 3, 5, 1, 6, 7], [1, 2, 0, 0, 2, 1, 0, 0])
 
-    @staticmethod
-    def get_turns():
-        return (Cube.d, Cube.dr, Cube.l, Cube.lr, Cube.b, Cube.br)
+
+def br(cube_tuple):
+    return turn_base(cube_tuple, [1, 5, 2, 3, 0, 4, 6, 7], [1, 2, 0, 0, 2, 1, 0, 0])
+
+
+def rotate(state, i, rotation):
+    state[i] = (state[i] + rotation) % 3
+
+
+def get_turns():
+    return (d, dr, l, lr, b, br)
 
 
 class States:
@@ -142,7 +129,7 @@ class States:
 
 
 def solve(cube):
-    forwards = States({cube.to_tuple(): 0})
+    forwards = States({cube: 0})
     backwards = States({SOLVED: 0})
     states = ((forwards, backwards), (backwards, forwards))
     while True:
@@ -153,7 +140,7 @@ def solve(cube):
                     return 14
             if other_states.has(state):
                 return depth + other_states.get_value(state)
-            for turn in Cube.get_turns():
+            for turn in get_turns():
                 new_state = turn(state)
                 if my_states.has(new_state):
                     continue
@@ -167,7 +154,7 @@ number_of_inputs = int(input())
 for i in range(0, number_of_inputs):
     inputs = [input() for j in range(0, 6)]
     old = time()
-    cube = Cube(i=inputs)
+    cube = get_cube(i=inputs)
     print(solve(cube))
     print('T : ' + str(time() - old))
     if i + 1 != number_of_inputs:
